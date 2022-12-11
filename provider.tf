79f7ff657af3e9f21e7013082736806d0b55db90
Misk77 <michelskoglund@hotmail.se>
2022-12-11 18:36:57 +0100

    test 7

---
+++ provider.tf
@@ -0,0 +1,14 @@
+### Main.tf ###
+
+terraform {
+  required_providers {
+    github = {
+      source  = "integrations/github"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "github" {
+  token = var.token # or `GITHUB_TOKEN`
+}
